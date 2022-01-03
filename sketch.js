f434a750893297409d70b992579bb70f649326c7
Soyeet <92953431+Soyeet@users.noreply.github.com>
2022-01-03 18:37:15 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var con2
+var ground;
+var ball2
+var top_wall;
+var ball;
+var con;
+var btn1;
+var btn2;
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+  }
+   
+  
+  btn2 = createImg('up.png');
+  btn2.position(20,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+  
+   
+  
+  
+
+  ground =new Ground(200,390,400,20);
+
+
+  ball = Bodies.circle(100,200,20,ball_options);
+  World.add(world,ball);
+  ball2 = Bodies.circle(100,300,20,ball_options);
+  World.add(world,ball2);
+   con=Matter.Constraint.create({
+    pointA:{ x:200,y:20},
+    pointB:{x:0,y:0},
+    bodyB:ball,
+    length:100,
+    stiffness:0.1,
+
+    
+
+  })
+  con2=Matter.Constraint.create({
+    pointA:{ x:200,y:20},
+    pointB:{x:0,y:0},
+    bodyB:ball2,
+    length:100,
+    stiffness:0.1,
+  });
+  World.add(world,con);  World.add(world,con2);
+
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+
+
+}
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  
+
+  ellipse(ball.position.x,ball.position.y,20);
+  ellipse(ball2.position.x,ball2.position.y,20);
+  ground.show();
+  push();
+  strokeWeight(2);
+  stroke(255);
+  line(con.pointA.x,con.pointA.y,ball.position.x,ball.position.y)
+  line(con2.pointA.x,con2.pointA.y,ball2.position.x,ball2.position.y)
+  pop();
+  Engine.update(engine);
+}
+
+
+function vForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+}
+  
+
+
